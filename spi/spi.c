e127b8f7549fb110c40d61c2bdf4b4a5acc16a8f
Nick Marchuk <ndm736@users.noreply.github.com>
2020-04-16 15:36:52 -0500

    spi code

---
+++ spi/spi.c
@@ -0,0 +1,22 @@
+// initialize SPI1
+void initSPI() {
+    // Pin B14 has to be SCK1
+    // Turn of analog pins
+    //...
+    // Make an output pin for CS
+    //...
+    //...
+    // Set SDO1
+    //...
+    // Set SDI1
+    //...
+
+    // setup SPI1
+    SPI1CON = 0; // turn off the spi module and reset it
+    SPI1BUF; // clear the rx buffer by reading from it
+    SPI1BRG = 1000; // 1000 for 12kHz, 1 for 12MHz; // baud rate to 10 MHz [SPI4BRG = (48000000/(2*desired))-1]
+    SPI1STATbits.SPIROV = 0; // clear the overflow bit
+    SPI1CONbits.CKE = 1; // data changes when clock goes from hi to lo (since CKP is 0)
+    SPI1CONbits.MSTEN = 1; // master operation
+    SPI1CONbits.ON = 1; // turn on spi 
+}
