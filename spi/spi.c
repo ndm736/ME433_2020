070e05f82cca0f22adb3b951aa4b500b22dde2ea
Nick Marchuk <ndm736@users.noreply.github.com>
2020-04-18 18:34:21 -0500

    edit brg comment

--- spi/spi.c
+++ spi/spi.c
@@ -14,7 +14,7 @@ void initSPI() {
     // setup SPI1
     SPI1CON = 0; // turn off the spi module and reset it
     SPI1BUF; // clear the rx buffer by reading from it
-    SPI1BRG = 1000; // 1000 for 12kHz, 1 for 12MHz; // baud rate to 10 MHz [SPI1BRG = (48000000/(2*desired))-1]
+    SPI1BRG = 1000; // 1000 for 24kHz, 1 for 12MHz; // baud rate to 10 MHz [SPI1BRG = (48000000/(2*desired))-1]
     SPI1STATbits.SPIROV = 0; // clear the overflow bit
     SPI1CONbits.CKE = 1; // data changes when clock goes from hi to lo (since CKP is 0)
     SPI1CONbits.MSTEN = 1; // master operation
