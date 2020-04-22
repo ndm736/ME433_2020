7e5b9744c27cbb9a5c04f35ba59af6abf3d97337
Nick Marchuk <ndm736@users.noreply.github.com>
2020-04-21 20:44:35 -0500

    i2c sampe code

---
+++ i2c/i2c_master_noint.c
@@ -0,0 +1,60 @@
+// I2C Master utilities, using polling rather than interrupts
+// The functions must be called in the correct order as per the I2C protocol
+// I2C pins need pull-up resistors, 2k-10k
+#include "i2c_master_noint.h"
+
+void i2c_master_setup(void) {
+    // using a large BRG to see it on the nScope, make it smaller after verifying that code works
+    // look up TPGD in the datasheet
+    I2C1BRG = 1000; // I2CBRG = [1/(2*Fsck) - TPGD]*Pblck - 2 (TPGD is the Pulse Gobbler Delay)
+    I2C1CONbits.ON = 1; // turn on the I2C1 module
+}
+
+void i2c_master_start(void) {
+    I2C1CONbits.SEN = 1; // send the start bit
+    while (I2C1CONbits.SEN) {
+        ;
+    } // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {
+    I2C1CONbits.RSEN = 1; // send a restart 
+    while (I2C1CONbits.RSEN) {
+        ;
+    } // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) { // send a byte to slave
+    I2C1TRN = byte; // if an address, bit 0 = 0 for write, 1 for read
+    while (I2C1STATbits.TRSTAT) {
+        ;
+    } // wait for the transmission to finish
+    if (I2C1STATbits.ACKSTAT) { // if this is high, slave has not acknowledged
+        // ("I2C1 Master: failed to receive ACK\r\n");
+        while(1){} // get stuck here if the chip does not ACK back
+    }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C1CONbits.RCEN = 1; // start receiving data
+    while (!I2C1STATbits.RBF) {
+        ;
+    } // wait to receive the data
+    return I2C1RCV; // read and return the data
+}
+
+void i2c_master_ack(int val) { // sends ACK = 0 (slave should send another byte)
+    // or NACK = 1 (no more bytes requested from slave)
+    I2C1CONbits.ACKDT = val; // store ACK/NACK in ACKDT
+    I2C1CONbits.ACKEN = 1; // send ACKDT
+    while (I2C1CONbits.ACKEN) {
+        ;
+    } // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) { // send a STOP:
+    I2C1CONbits.PEN = 1; // comm is complete and master relinquishes bus
+    while (I2C1CONbits.PEN) {
+        ;
+    } // wait for STOP to complete
+}
